a29f3bcdca09b430d0fb8f12fc843e03e3faf9a9
gerard33 <gerard33@users.noreply.github.com>
2017-01-29 22:59:04 +0100

    Update MediaPlayer.js

--- src/widgets/MediaPlayer.js
+++ src/widgets/MediaPlayer.js
@@ -41,7 +41,7 @@ class MediaPlayer extends Component {
 
   render() {
     const valueText = this.props.valueText || (this.props.value === 0 ? 'Off' : 'On');
-	const valuePlaying = ((this.props.playing === "100" && valueText === "On") ? 'Loading...' : ((this.props.playing === "100" && valueText === "Off") ? 'Off' : this.props.playing));
+    const valuePlaying = ((this.props.playing === "100" && valueText === "On") ? 'Loading...' : ((this.props.playing === "100" && valueText === "Off") ? 'Off' : this.props.playing));
     return (
 	  <button className="switch" style={this.getButtonStyle()} onClick={this.handleClick} title={valueText}>{this.props.label}<br/><i className="playing">{valuePlaying}</i></button>
     );
