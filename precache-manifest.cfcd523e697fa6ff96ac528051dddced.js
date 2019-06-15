d41993043da6db06ef4b9aade1522d34131f7ec7
t0mg <t0mg@users.noreply.github.com>
2019-06-15 20:48:13 +0200

    Updated deps to clear potential vulnerabilities

---
+++ precache-manifest.cfcd523e697fa6ff96ac528051dddced.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d71ad033e9de10636a723ebcb672ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0511facafa9b4c9cd18e",
+    "url": "/static/css/2.716a4a07.chunk.css"
+  },
+  {
+    "revision": "01491fab2c02c3cbc90b",
+    "url": "/static/css/main.f34277ed.chunk.css"
+  },
+  {
+    "revision": "0511facafa9b4c9cd18e",
+    "url": "/static/js/2.8c19d5ab.chunk.js"
+  },
+  {
+    "revision": "01491fab2c02c3cbc90b",
+    "url": "/static/js/main.d59f3752.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "705a457245bd0b829427169ea25ea7b4",
+    "url": "/static/media/1.705a4572.png"
+  },
+  {
+    "revision": "0ad13601d1d48833b278e9921f3147b7",
+    "url": "/static/media/2.0ad13601.png"
+  },
+  {
+    "revision": "97ad25995c33f2fbc261d4a0b2d67c3e",
+    "url": "/static/media/4.97ad2599.png"
+  }
+]);
